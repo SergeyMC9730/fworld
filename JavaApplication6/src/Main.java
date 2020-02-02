643d67ee0d252df482ddf55d025dc5fc723b4325
SergeyMC9730 <SergeyMC9730@SergeyMC9730-PC>
2020-02-02 12:22:56 +0800

    FWorld Alpha 0.1

---
+++ JavaApplication6/src/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SergeyMC9730
+ */
+public class Main {
+    public static void Main(String[] args){
+        System.out.println(223 + 223);
+    }
+    
+}
